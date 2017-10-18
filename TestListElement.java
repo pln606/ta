cc59d4f68fa3e741ab69471bd6b9a7ef69436448
kellyholtzman <kellyholtzman@172.20.10.2>
2017-10-18 01:47:13 -0600

    Implement test Class TestListElement.

---
+++ TestListElement.java
@@ -0,0 +1,13 @@
+
+import listelement.ListElement;
+
+public class TestListElement {
+	
+	public static void main(String args[]) {
+		
+		ListElement le = new ListElement();
+		le.setData(5);
+		
+		System.out.println("Initializing ListELement..." + "\n" + "ListElement at [0] has data: " + le.getData());
+	}
+}
